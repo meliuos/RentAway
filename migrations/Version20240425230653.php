863e1a0c1a5231b83ac34723c27202e07680743e
meliuos <mootez.souilem@insat.ucar.tn>
2024-04-26 19:06:05 +0100

    fixed the webcore encore issue

--- migrations/Version20240425230653.php
+++ migrations/Version20240425230653.php
@@ -7,9 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
+
 final class Version20240425230653 extends AbstractMigration
 {
     public function getDescription(): string
