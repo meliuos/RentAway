935324ff772e4cde589af29e9f762e406c2d4aaa
yasmine-sassi <yasmine.sassi@insat.ucar.tn>
2024-04-29 22:46:02 +0100

    admin dashboard interface

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,22 +2,28 @@
 
 namespace App\Controller;
 
+use App\Repository\UsersRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'admin')]
-    public function index(Request $request): Response
+    public function index(SessionInterface $session,UsersRepository $usersRepository): Response
     {
-        if(($request->getSession()->get('email') == null) || ($request->getSession()->get('amin') != '1'))
+        $session->start();
+        $email=$session->get('email');
+        if($email== null )
         {
-            return $this->redirectToRoute('HomeController');
+            return $this->redirectToRoute('login');
         }
+        $users = $usersRepository->findAll();
         return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+            'users' => $users,
         ]);
     }
+
 }
