159976e38f9bdb694d0974dc411260fbc42ee8a3
yasmine-sassi <yasmine.sassi@insat.ucar.tn>
2024-05-02 11:01:47 +0100

    added delete and edit buttons in manage post

--- src/Controller/ManagePostController.php
+++ src/Controller/ManagePostController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Apart;
 use App\Repository\ApartRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
@@ -23,6 +25,23 @@ class ManagePostController extends AbstractController
         $aparts = $apartRepository->findBy(['mail' => $email]);
         return $this->render('manage_post/index.html.twig', [
             'aparts' => $aparts,
+        ]);
+    }
+    #[Route('/edit/delete/{id<\d+>}', name: 'delete_post')]
+    public function delete(ApartRepository $apartRepository,$id,ManagerRegistry $doctrine): RedirectResponse
+    {
+        $apart = $apartRepository->find($id);
+        $entityManager = $doctrine->getManager();
+        $entityManager->remove($apart);
+        $entityManager->flush();
+        return $this->redirectToRoute('app_manage_post');
+    }
+    #[Route('/edit/update/{id<\d+>}', name: 'update_post')]
+    public function update(ApartRepository $apartRepository,$id,ManagerRegistry $doctrine): Response
+    {
+        $apart = $apartRepository->find($id);
+        return $this->render('manage_post/update.html.twig', [
+            'apart' => $apart,
         ]);
     }
 }
